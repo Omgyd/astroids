cfc41b5325a31f24a660be79c7e6a3b01d6570b4
Benjamin Lowry <Lowry_46@hotmail.com>
2025-05-14 13:38:54 -0400

    adding clock and tick to set fps for game.

--- main.py
+++ main.py
@@ -4,16 +4,22 @@ from constants import SCREEN_WIDTH, SCREEN_HEIGHT, ASTEROID_KINDS, ASTEROID_MAX_
 
 def main():
     pygame.init()
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+    clock = pygame.time.Clock()
+    dt = 0
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
+    
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
         screen.fill("black")
         pygame.display.flip()
+
+        # limits the framerate to 60 fps
+        dt = clock.tick(60) / 1000
         
 
 if __name__ == "__main__":
