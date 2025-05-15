c78a3e5a6d8e50fa9e46b421ea3118ea623985ec
Benjamin Lowry <Lowry_46@hotmail.com>
2025-05-15 10:55:55 -0400

    created player class and added methods to draw and move the ship

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
-from constants import SCREEN_WIDTH, SCREEN_HEIGHT, ASTEROID_KINDS, ASTEROID_MAX_RADIUS, ASTEROID_MIN_RADIUS, ASTEROID_SPAWN_RATE
+from constants import *
+from player import Player
 
 
 def main():
@@ -8,19 +9,22 @@ def main():
     dt = 0
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     
-    
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-            
+
+        player.update(dt) 
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
 
         # limits the framerate to 60 fps
         dt = clock.tick(60) / 1000
-        
 
 if __name__ == "__main__":
     main()
